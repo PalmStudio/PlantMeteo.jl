38f0c2f05ee8bb34cce88b7416a38b4aa797f872
Rémi Vezy <VEZY@users.noreply.github.com>
2022-12-12 16:52:56 +0100

    Add conversions

--- src/PlantMeteo.jl
+++ src/PlantMeteo.jl
@@ -8,10 +8,12 @@ include("variables_computations.jl")
 include("emissivity.jl")
 include("atmosphere.jl")
 include("weather.jl")
+include("conversions.jl")
 
 export Atmosphere, Weather, Constants
 export atmosphere_emissivity, vapor_pressure
 export e_sat, air_density, latent_heat_vaporization
 export psychrometer_constant
+export rh_from_vpd, rh_from_e, vpd
 
 end
