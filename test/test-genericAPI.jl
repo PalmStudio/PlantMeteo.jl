545e7c423abd2273c2acb1da6a18d4b1e445f292
Rémi Vezy <VEZY@users.noreply.github.com>
2023-01-02 15:46:24 +0100

    Add to_daily and some tests

--- test/test-genericAPI.jl
+++ test/test-genericAPI.jl
@@ -11,24 +11,24 @@ vars = (
 struct TestAPI <: PlantMeteo.AbstractAPI end
 
 function PlantMeteo.get_forecast(params::TestAPI, lat, lon, period; verbose=true)
+    period = Dates.DateTime(period[1]):Dates.Hour(1):Dates.DateTime(period[end])+Dates.Hour(23)
+
+    atms = Atmosphere[]
+    for i in period
+        T = 20.0 + 0.1 * Dates.value(Dates.Hour(i - period[1]))
+        Wind = T - 19.9
+        push!(atms, Atmosphere(
+            date=i,
+            duration=Hour(1),
+            T=T,
+            Wind=Wind,
+            P=101.0,
+            Rh=0.6,
+        ))
+    end
+
     TimeStepTable(
-        [
-            Atmosphere(
-                date=DateTime(period[1]),
-                duration=Hour(1),
-                T=20.0,
-                Wind=1.0,
-                P=101.0,
-                Rh=0.6,
-            ),
-            Atmosphere(
-                date=DateTime(period[end]),
-                duration=Hour(1),
-                T=23.0,
-                Wind=1.0,
-                P=101.0,
-                Rh=0.6,
-            )],
+        atms,
         (
             latitude=lat,
             longitude=lon,
@@ -42,19 +42,65 @@ end
     sink = TimeStepTable
 
     w = get_weather(lat, lon, period; api=api, sink=sink)
-    @test length(w) == 2 # 2 x 24 hours
+    @test length(w) == 48 # 2 x 24 hours
     @test typeof(w) <: TimeStepTable{A} where {A<:Atmosphere}
     @test typeof(w.T) == Vector{Float64}
     @test keys(w) == vars
 end
 
-@testset "Generic APi sink" begin
+@testset "Generic API sink" begin
     period = Dates.today():Dates.Day(1):Dates.today()+Dates.Day(1)
     api = TestAPI()
     sink = DataFrame
 
     w = get_weather(lat, lon, period; api=api, sink=sink)
     @test typeof(w) <: DataFrame
-    @test nrow(w) == 2 # 2 x 24 hours
+    @test nrow(w) == 48 # 2 x 24 hours
     @test names(w) == [string.(vars)...]
+end
+
+
+@testset "to_daily" begin
+    period = Dates.today():Dates.Day(1):Dates.today()+Dates.Day(2)
+    api = TestAPI()
+    w = get_weather(lat, lon, period; api=api, sink=TimeStepTable)
+    w_daily = to_daily(w)
+
+    w_dates = unique(Dates.Date.(w.date))
+    @test length(w_dates) == 3
+    @test w_dates == w_daily.date
+
+    @test w_daily.year == [Dates.year(w.date[Dates.Date.(w.date).==i][1]) for i in w_dates]
+    @test w_daily.duration == [sum(w.duration[Dates.Date.(w.date).==i]) for i in w_dates]
+    @test w_daily.T ≈ [Statistics.mean(w.T[Dates.Date.(w.date).==i]) for i in w_dates] atol = 1.0e-6
+    @test w_daily.Tmin ≈ [minimum(w.T[Dates.Date.(w.date).==i]) for i in w_dates] atol = 1.0e-6
+    @test w_daily.Tmax ≈ [maximum(w.T[Dates.Date.(w.date).==i]) for i in w_dates] atol = 1.0e-6
+    @test w_daily.Wind ≈ [Statistics.mean(w.Wind[Dates.Date.(w.date).==i]) for i in w_dates] atol = 1.0e-6
+    @test w_daily.P ≈ [Statistics.mean(w.P[Dates.Date.(w.date).==i]) for i in w_dates] atol = 1.0e-6
+    @test w_daily.Precipitations ≈ [Statistics.mean(w.Precipitations[Dates.Date.(w.date).==i]) for i in w_dates] atol = 1.0e-6
+    @test w_daily.Rh ≈ [Statistics.mean(w.Rh[Dates.Date.(w.date).==i]) for i in w_dates] atol = 1.0e-6
+end
+
+
+@testset "to_daily (with user transformations)" begin
+    period = Dates.today():Dates.Day(1):Dates.today()+Dates.Day(2)
+    api = TestAPI()
+    w = get_weather(lat, lon, period; api=api, sink=TimeStepTable)
+    w_daily = to_daily(
+        w,
+        :T => minimum => :Tmin, # override default transformation with same transformation
+        :T => (x -> maximum(x) .+ 0.1) => :Tmax, # override default transformation with different transformation
+        :T => maximum, # override default transformation but not renaming again (makes T_maximum)
+        :T => (x -> sum(max.(x .- 15.0, 0.0))) => :GDD, # add a new variable
+    )
+
+    # With user transformations:
+    w_dates = unique(Dates.Date.(w.date))
+    @test length(w_dates) == 3
+    @test w_dates == w_daily.date
+
+    @test w_daily.Tmin ≈ [minimum(w.T[Dates.Date.(w.date).==i]) for i in w_dates] atol = 1.0e-6
+    @test w_daily.Tmax ≈ [maximum(w.T[Dates.Date.(w.date).==i]) + 0.1 for i in w_dates] atol = 1.0e-6
+    @test w_daily.T_maximum ≈ [maximum(w.T[Dates.Date.(w.date).==i]) for i in w_dates] atol = 1.0e-6
+    @test w_daily.GDD ≈ [sum(max.(w.T[Dates.Date.(w.date).==i] .- 15.0, 0.0)) for i in w_dates] atol = 1.0e-6
 end
