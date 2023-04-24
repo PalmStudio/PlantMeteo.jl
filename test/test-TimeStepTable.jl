63aff1b03846d2254837a6561805b41e8623b166
Rémi Vezy <VEZY@users.noreply.github.com>
2023-04-24 18:25:03 +0200

    Update test-TimeStepTable.jl
    Add a lot more tests on the TimeStepTable + TimeStepRow, with three kinds of base structures: Dict, NamedTuple and Atmosphere

--- test/test-TimeStepTable.jl
+++ test/test-TimeStepTable.jl
@@ -1,55 +1,63 @@
-@testset "Testing TimeStepTable{Atmosphere}" begin
-    vars = Atmosphere(T=20.0, Wind=1.0, P=101.3, Rh=0.65)
-    ts = TimeStepTable([vars, vars])
+ref_values = (T=20.0, Wind=1.0, P=101.3, Rh=0.65)
+row_types = [
+    Atmosphere(; ref_values...),
+    Dict(zip(keys(ref_values), values(ref_values))),
+    ref_values
+]
 
-    @test Tables.istable(typeof(ts))
+for row_type in row_types
+    @testset "Testing TimeStepTable{$(nameof(typeof(row_type)))}}" begin
+        # row_type = row_types[2]
+        ts = TimeStepTable([row_type, row_type])
 
-    ts_rows = Tables.rows(ts)
-    @test length(ts_rows) == length(ts)
+        @test Tables.istable(typeof(ts))
 
-    @test Tables.rowaccess(typeof(ts))
-    # test that it defines column access
-    ts_first = first(ts)
-    @test eltype(ts) == typeof(ts_first)
-    # now we can test our `Tables.AbstractRow` interface methods on our MatrixRow
-    @test ts_first.T == 20.00
-    @test Tables.getcolumn(ts_first, :Wind) == 1.0
-    @test Tables.getcolumn(ts_first, 1) == ts_first[:date]
-    @test keys(ts) == propertynames(ts_first) == (
-              :date, :duration, :T, :Wind, :P, :Rh, :Precipitations,
-              :Cₐ, :e, :eₛ, :VPD, :ρ, :λ, :γ, :ε, :Δ, :clearness,
-              :Ri_SW_f, :Ri_PAR_f, :Ri_NIR_f, :Ri_TIR_f, :Ri_custom_f
-          )
+        ts_rows = Tables.rows(ts)
+        @test length(ts_rows) == length(ts)
 
-    # Get column value using getcolumn:
-    @test Tables.getcolumn(ts_rows[1], 1) == vars[1]
-    @test Tables.getcolumn(ts_rows[1], :Rh) == vars.Rh
+        @test Tables.rowaccess(typeof(ts))
+        # test that it defines column access
+        ts_first = first(ts)
+        @test eltype(ts) == typeof(ts_first)
+        # now we can test our `Tables.AbstractRow` interface methods on our MatrixRow
+        @test ts_first.T == 20.00
+        @test Tables.getcolumn(ts_first, :Wind) == 1.0
+        @test Tables.getcolumn(ts_rows[1], :Rh) == getindex(row_type, :Rh)
 
-    # Get column value using indexing and/or the dot syntax:
-    @test ts_rows[1].P == vars.P
-    @test ts_rows[1][2] == vars[2]
-    @test ts_rows[1][:date] == vars[1]
+        if isa(row_type, Atmosphere)
+            @test Tables.getcolumn(ts_first, 1) == ts_first[:date]
+            @test Tables.getcolumn(ts_rows[1], 1) == row_type[1]
+        end
 
-    # Get column values for all rows at once:
-    cols = Tables.columns(ts)
-    @test ts.T == cols.T
-    @test ts[1, 1] == cols.date[1]
+        @test keys(ts) == propertynames(ts_first) == names(ts)
 
-    # Indexing as a Matrix:
-    @test ts[1, :] == ts_first
-    @test ts[:, 1] == cols.date
+        # Get column value using indexing and/or the dot syntax:
+        @test ts_rows[1].P == ref_values.P
 
-    # Get column names:
-    @test Tables.columnnames(ts) == keys(vars)
+        @test ts_rows[1][2] == getindex(row_type, names(ts)[2])
+        @test ts_rows[1][:P] == ref_values[:P]
 
-    # Get column names for a single row:
-    @test Tables.columnnames(ts_rows[1]) == keys(vars)
+        # Get column values for all rows at once:
+        cols = Tables.columns(ts)
+        @test ts.T == cols.T
+        @test ts[1, 1] == getindex(cols, names(ts)[1])[1]
 
-    # Testing transforming into a DataFrame:
-    df = DataFrame(ts)
+        # Indexing as a Matrix:
+        @test ts[1, :] == ts_first
+        @test ts[:, 1] == getindex(cols, names(ts)[1])
 
-    @test size(df) == (2, 22)
-    @test df.T == [20.0, 20.0]
-    @test df.VPD == [0.8214484239448965, 0.8214484239448965]
-    @test names(df) == [string.(keys(vars))...]
+        # Get column names:
+        @test Tables.columnnames(ts) == (keys(row_type)...,)
+
+        # Get column names for a single row:
+        @test Tables.columnnames(ts_rows[1]) == (keys(row_type)...,)
+
+        # Testing transforming into a DataFrame:
+        df = DataFrame(ts)
+
+        @test size(df) == (nrow(ts), ncol(ts))
+        @test df.T == [20.0, 20.0]
+        @test df.Rh == [0.65, 0.65]
+        @test names(df) == [string.(keys(row_type))...]
+    end
 end
